493639bece1db07181dd6248341ebeaa201122c9
Dylan Baker <dylang.baker@googlemail.com>
2019-05-27 11:02:14 +0100

    add print_results, more mocking

--- fonz/tests/test_connection.py
+++ fonz/tests/test_connection.py
@@ -1,40 +1,66 @@
 import pytest
 import requests
 import requests_mock
+from fonz.tests import mock
 from fonz import connection
 
 base = 'https://test.looker.com'
 
+client = connection.Fonz(
+    url=base,
+    client_id='client_id',
+    client_secret='client_secret',
+    port=19999,
+    api='3.0',
+    project='test_project')
+
 
 def test_get_explores():
-
-    client = connection.Fonz(
-        url=base,
-        client_id='client_id',
-        client_secret='client_secret',
-        port=19999,
-        api='3.0',
-        project='test_project')
-
-    response = """[
-        {
-        "name": "model_one",
-        "project_name": "test_project",
-        "explores": [{"name": "explore_one"}, {"name": "explore_two"}]
-        }
-    ]"""
 
     output = [
         {'model': 'model_one', 'explore': 'explore_one'},
         {'model': 'model_one', 'explore': 'explore_two'}
         ]
 
-    with requests_mock.mock() as m:
-        m.get(
-            url="{}{}".format(client.url, 'lookml_models'),
-            text=response
-            )
+    with mock.looker_mock as m:
 
-        explores = client.get_explores()
+        response = client.get_explores()
+        assert response == output
 
-        assert explores == output
+
+def test_get_explore_dimensions():
+
+    body = {'model': 'model_one', 'explore': 'explore_one'}
+    output = ['dimension_one', 'dimension_two']
+
+    with mock.looker_mock as m:
+
+        response = client.get_explore_dimensions(body)
+        assert response == output
+
+
+def test_get_dimensions():
+
+    explores = [
+        {'model': 'model_one', 'explore': 'explore_one'},
+        {'model': 'model_one', 'explore': 'explore_two'}
+        ]
+
+    output = [
+        {
+            'model': 'model_one',
+            'explore': 'explore_one',
+            'dimensions': ['dimension_one', 'dimension_two']
+        },
+        {
+            'model': 'model_one',
+            'explore': 'explore_two',
+            'dimensions': ['dimension_three', 'dimension_four']
+        }
+    ]
+
+    with mock.looker_mock as m:
+
+        response = client.get_dimensions(explores)
+        print(response)
+        assert response == output
