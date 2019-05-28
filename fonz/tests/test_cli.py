e0b91733c5a60da0e71ab82a9c552f38623539e7
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-05-28 17:17:43 -0400

    Use a mocked version of Fonz and test how it's called.

--- fonz/tests/test_cli.py
+++ fonz/tests/test_cli.py
@@ -1,7 +1,8 @@
 import os
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 import pytest
 from click.testing import CliRunner
+from fonz.tests.constants import TEST_BASE_URL
 from fonz.cli import connect
 import logging
 
@@ -22,31 +23,39 @@ class TestConnect(object):
         result = self.runner.invoke(connect)
         assert result.exit_code != 0
 
-    @patch("fonz.connection.Fonz.connect")
-    def test_with_command_line_args_only(self, mock_connect):
+    @patch("fonz.cli.Fonz", autospec=True)
+    def test_with_command_line_args_only(self, mock_client):
         result = self.runner.invoke(
             connect,
             [
-                "https://test.looker.com",
+                TEST_BASE_URL,
                 "--client-id",
                 "FAKE_CLIENT_ID",
                 "--client-secret",
                 "FAKE_CLIENT_SECRET",
             ],
         )
+        mock_client.assert_called_once_with(
+            TEST_BASE_URL, "FAKE_CLIENT_ID", "FAKE_CLIENT_SECRET", 19999, "3.0"
+        )
+        mock_client.return_value.connect.assert_called_once()
         assert result.exit_code == 0
 
-    @patch("fonz.connection.Fonz.connect")
+    @patch("fonz.cli.Fonz", autospec=True)
     @patch.dict(
         os.environ,
         {
-            "LOOKER_BASE_URL": "https://test.looker.com",
+            "LOOKER_BASE_URL": TEST_BASE_URL,
             "LOOKER_CLIENT_ID": "FAKE_CLIENT_ID",
             "LOOKER_CLIENT_SECRET": "FAKE_CLIENT_SECRET",
         },
     )
-    def test_with_env_vars_only(self, mock_connect):
+    def test_with_env_vars_only(self, mock_client):
         result = self.runner.invoke(connect)
+        mock_client.assert_called_once_with(
+            TEST_BASE_URL, "FAKE_CLIENT_ID", "FAKE_CLIENT_SECRET", 19999, "3.0"
+        )
+        mock_client.return_value.connect.assert_called_once()
         assert result.exit_code == 0
 
     def test_with_config_file_only(self):
