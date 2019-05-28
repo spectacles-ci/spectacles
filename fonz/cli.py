7018bcce684c8809577e9c700c5955cc91e1a82b
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-05-28 15:55:15 -0400

    Make client ID and secret required options instead of arguments.

--- fonz/cli.py
+++ fonz/cli.py
@@ -9,8 +9,8 @@ def cli():
 
 @click.command()
 @click.argument("url", envvar="LOOKER_BASE_URL")
-@click.argument("client_id", envvar="LOOKER_CLIENT_ID")
-@click.argument("client_secret", envvar="LOOKER_CLIENT_SECRET")
+@click.option("--client-id", required=True, envvar="LOOKER_CLIENT_ID")
+@click.option("--client-secret", required=True, envvar="LOOKER_CLIENT_SECRET")
 @click.option("--port", default=19999)
 @click.option("--api", default="3.0")
 def connect(url, client_id, client_secret, port, api):
