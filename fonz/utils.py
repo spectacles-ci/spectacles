9122a02196dba1a08041c1704e44fd4f5e1759ec
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-05-29 00:55:23 -0400

    Simplify compose_url function and adjust tests accordingly. Add constants file.

--- fonz/utils.py
+++ fonz/utils.py
@@ -1,25 +1,10 @@
 from typing import Sequence, List, Dict, Any, Union
+from urllib.parse import urljoin
 
 JsonDict = Dict[str, Any]
 
 
-def compose_url(
-    url_base: str,
-    endpoint: str,
-    endpointid: Union[str, int] = None,
-    subendpoint: str = None,
-    subendpointid: Union[str, int] = None,
-) -> str:
-
-    url = "{}{}".format(url_base, endpoint)
-
-    if endpointid:
-        url += "/" + str(endpointid)
-
-        if subendpoint:
-            url += "/" + subendpoint
-
-            if subendpointid:
-                url += "/" + str(subendpointid)
-
+def compose_url(base_url: str, path: List) -> str:
+    parts = [base_url] + path
+    url = "/".join(str(part).strip("/") for part in parts)
     return url
