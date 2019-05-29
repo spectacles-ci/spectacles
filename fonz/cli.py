6bf4414df0da773226a279b8811b50df91536b5e
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-05-29 11:13:20 -0400

    Switch arguments to options and add more environment variable options.

--- fonz/cli.py
+++ fonz/cli.py
@@ -34,16 +34,17 @@ def connect(base_url, client_id, client_secret, config_file, port, api):
     client.connect()
 
 
-@click.command()
-@click.argument("project")
-@click.argument("branch")
-@click.argument("url", envvar="LOOKER_BASE_URL")
-@click.argument("client_id", envvar="LOOKER_CLIENT_ID")
-@click.argument("client_secret", envvar="LOOKER_CLIENT_SECRET")
+@click.command(cls=CommandWithConfig)
+@click.option("--project", envvar="LOOKER_PROJECT")
+@click.option("--branch", envvar="LOOKER_GIT_BRANCH")
+@click.option("--base-url", envvar="LOOKER_BASE_URL")
+@click.option("--client-id", envvar="LOOKER_CLIENT_ID")
+@click.option("--client-secret", envvar="LOOKER_CLIENT_SECRET")
+@click.option("--config-file")
 @click.option("--port", default=19999)
 @click.option("--api", default="3.0")
-def sql(url, client_id, client_secret, port, api, project, branch):
-    client = Fonz(url, client_id, client_secret, port, api, project, branch)
+def sql(project, branch, base_url, client_id, client_secret, config_file, port, api):
+    client = Fonz(base_url, client_id, client_secret, port, api, project, branch)
     client.connect()
     client.update_session()
     explores = client.get_explores()
