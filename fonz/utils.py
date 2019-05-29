5d4d6517276724ea0dc45801e6d89f0509876a90
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-05-29 10:37:49 -0400

    Raise TypeError for non-list paths.

--- fonz/utils.py
+++ fonz/utils.py
@@ -4,7 +4,9 @@ from urllib.parse import urljoin
 JsonDict = Dict[str, Any]
 
 
-def compose_url(base_url: str, path: List) -> str:
+def compose_url(base_url: str, path: List[str]) -> str:
+    if not isinstance(path, list):
+        raise TypeError("URL path must be a list")
     parts = [base_url] + path
     url = "/".join(str(part).strip("/") for part in parts)
     return url
