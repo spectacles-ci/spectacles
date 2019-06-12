66eb8dfb8eaf95ecb4562d6bb27e5882fa3f28e5
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-12 15:14:55 -0500

    Add failing test for sql_context utility.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,3 +15,10 @@ def test_compose_url_multiple_path_components():
 def test_compose_url_with_extra_slashes():
     url = utils.compose_url(TEST_BASE_URL + "/", ["/api//", "3.0/login/"])
     assert url == "https://test.looker.com/api/3.0/login"
+
+
+def test_extract_sql_context():
+    text = "\n".join([f"{n}" for n in range(1, 21)])
+    expected_result = "| 4\n| 5\n* 6\n| 7\n| 8"
+    result = utils.extract_sql_context(sql=text, line_number=6, window_size=2)
+    assert result == expected_result
