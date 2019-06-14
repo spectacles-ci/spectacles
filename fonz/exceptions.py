b117f42ab1f59e76e3a3f29c8465f44ed113b2d4
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-14 13:54:27 -0500

    Move validation logic from CLI into Fonz.

--- fonz/exceptions.py
+++ fonz/exceptions.py
@@ -6,6 +6,10 @@ class ConnectionError(FonzException):
     exit_code = 101
 
 
+class ValidationError(FonzException):
+    exit_code = 102
+
+
 class SqlError(FonzException):
     def __init__(self, query_id, explore_name, message):
         self.query_id = query_id
