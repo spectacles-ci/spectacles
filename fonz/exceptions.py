e7f7662cd6cd6240881be01fdd2e7967d8e21567
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-16 00:22:39 -0400

    Remove SqlError.

--- fonz/exceptions.py
+++ fonz/exceptions.py
@@ -8,13 +8,3 @@ class ConnectionError(FonzException):
 
 class ValidationError(FonzException):
     exit_code = 102
-
-
-class SqlError(FonzException):
-    def __init__(self, query_id, message, url=None):
-        self.query_id = query_id
-        self.message = message
-        self.url = url
-
-    def __str__(self):
-        return repr(self.message)
