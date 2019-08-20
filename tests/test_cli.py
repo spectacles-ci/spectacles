515ea94779a2faea2e7fe4f2cce580995a56916d
Dylan Baker <dylang.baker@googlemail.com>
2019-08-20 20:58:10 +0100

    finally making testing progresss

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from unittest.mock import patch, Mock
 from unittest import TestCase
 import yaml
@@ -6,7 +7,7 @@ import pytest
 import click
 from click.testing import CliRunner
 from tests.constants import TEST_BASE_URL, ENV_VARS
-from fonz.cli import create_parser
+from fonz.cli import create_parser, main
 import logging
 
 
@@ -40,14 +41,84 @@ def parser():
 def test_help(parser):
     with pytest.raises(SystemExit) as cm:
         parsed = parser.parse_args(["--help"])
+        assert cm.value.code == 0
 
 
-def test_something(parser):
-    parsed = parser.parse_args(["connect", "--base-url", "url"])
-    assert parsed.base_url == "url"
+@patch("fonz.cli.connect", autospec=True)
+def test_connect_with_base_cli(mock_connect, clean_env):
+    with patch.object(
+        sys,
+        "argv",
+        [
+            "fonz",
+            "connect",
+            "--base-url",
+            "cli_url",
+            "--client-id",
+            "cli_client_id",
+            "--client-secret",
+            "cli_client_secret",
+        ],
+    ):
+        main()
+        mock_connect.assert_called_once_with(
+            "cli_url", "cli_client_id", "cli_client_secret", 19999, 3.0
+        )
 
 
-def test_something_else(env, parser):
-    parsed = parser.parse_args(["connect", "--base-url", "url"])
-    assert parsed.base_url == "url"
-    assert parsed.client_secret == "CLIENT_SECRET_ENV_VAR"
+@patch("fonz.cli.connect", autospec=True)
+def test_connect_with_full_cli(mock_connect, clean_env):
+    with patch.object(
+        sys,
+        "argv",
+        [
+            "fonz",
+            "connect",
+            "--base-url",
+            "cli_url",
+            "--client-id",
+            "cli_client_id",
+            "--client-secret",
+            "cli_client_secret",
+            "--port",
+            "272727",
+            "--api-version",
+            "3.1",
+        ],
+    ):
+        main()
+        mock_connect.assert_called_once_with(
+            "cli_url", "cli_client_id", "cli_client_secret", "272727", "3.1"
+        )
+
+
+@patch("fonz.cli.connect", autospec=True)
+def test_connect_with_env_variables(mock_connect, env):
+    with patch.object(sys, "argv", ["fonz", "connect"]):
+        main()
+        mock_connect.assert_called_once_with(
+            "https://test.looker.com",
+            "CLIENT_ID_ENV_VAR",
+            "CLIENT_SECRET_ENV_VAR",
+            19999,
+            3.0,
+        )
+
+
+@patch("fonz.cli.yaml.load")
+@patch("fonz.cli.connect", autospec=True)
+def test_connect_with_config_file(mock_connect, mock_yaml_load, clean_env):
+    mock_yaml_load.return_value = {
+        "base_url": TEST_BASE_URL,
+        "client_id": "CLIENT_ID_CONFIG",
+        "client_secret": "CLIENT_SECRET_CONFIG",
+    }
+    with patch.object(sys, "argv", ["fonz", "connect", "--config-file", "config.yml"]):
+        main()
+        mock_connect.assert_called_once_with(
+            "https://test.looker.com",
+            "CLIENT_ID_CONFIG",
+            "CLIENT_SECRET_CONFIG",
+            19999,
+            3.0,
+        )
