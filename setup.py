d68dd8578279e85f6c7172fae03fc4baa1d62dcb
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-08-28 18:06:48 -0400

    Bump version from 0.0.1 > 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fonz",
-    version="0.0.1",
+    version="0.1.0",
     py_modules=["fonz"],
     packages=find_packages(),
     include_package_data=True,
