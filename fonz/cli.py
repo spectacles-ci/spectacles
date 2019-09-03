39b49c038b8ea95f82540dc340a676458a000baf
Dylan Baker <dylang.baker@googlemail.com>
2019-09-03 19:00:40 +0100

    click -> argparse 2: this time is parsenal (#52)
    * CLI changes -> argparse:
    
    * fixing requirements
    
    * initial new cli tests:
    
    * finally making testing progresss
    
    * more tests
    
    * remove autospec, move sys.argv to decorator, fix port and api version string type

--- fonz/cli.py
+++ fonz/cli.py
@@ -1,6 +1,7 @@
 import sys
-import click
 import yaml
+import argparse
+import os
 from fonz.connection import Fonz
 from fonz.exceptions import FonzException, ValidationError
 from fonz.logger import GLOBAL_LOGGER as logger, LOG_FILEPATH
@@ -33,58 +34,104 @@ def handle_exceptions(function):
     return wrapper
 
 
-class CommandWithConfig(click.Command):
-    @handle_exceptions
-    def invoke(self, ctx):
-        config_filename = ctx.params.get("config_file")
-        if config_filename is not None:
-            with open(config_filename) as file:
-                config = yaml.safe_load(file)
-                for param, value in ctx.params.items():
-                    if value is None and param in config:
-                        ctx.params[param] = config[param]
+@handle_exceptions
+def main():
+    parser = create_parser()
+    args = parse_args(parser)
 
-        return super(CommandWithConfig, self).invoke(ctx)
+    if args.command == "connect":
+        connect(
+            args.base_url,
+            args.client_id,
+            args.client_secret,
+            args.port,
+            args.api_version,
+        )
+    elif args.command == "sql":
+        sql(
+            args.project,
+            args.branch,
+            args.base_url,
+            args.client_id,
+            args.client_secret,
+            args.port,
+            args.api_version,
+            args.batch,
+        )
 
 
-@click.group()
-def cli():
-    pass
+def create_parser():
+    parser = argparse.ArgumentParser(prog="fonz")
+
+    subs = parser.add_subparsers(title="Available sub-commands", dest="command")
+    base_subparser = _build_base_subparser()
+
+    connect_sub = _build_connect_subparser(subs, base_subparser)
+    sql_sub = _build_sql_subparser(subs, base_subparser)
+
+    return parser
 
 
-@click.command(cls=CommandWithConfig)
-@click.option("--base-url", envvar="LOOKER_BASE_URL")
-@click.option("--client-id", envvar="LOOKER_CLIENT_ID")
-@click.option("--client-secret", envvar="LOOKER_CLIENT_SECRET")
-@click.option("--config-file")
-@click.option("--port", default=19999)
-@click.option("--api-version", default="3.0")
-def connect(base_url, client_id, client_secret, config_file, port, api_version):
+def _build_base_subparser():
+    base_subparser = argparse.ArgumentParser(add_help=False)
+
+    base_subparser.add_argument("--base-url", default=os.environ.get("LOOKER_BASE_URL"))
+    base_subparser.add_argument(
+        "--client-id", default=os.environ.get("LOOKER_CLIENT_ID")
+    )
+    base_subparser.add_argument(
+        "--client-secret", default=os.environ.get("LOOKER_CLIENT_SECRET")
+    )
+    base_subparser.add_argument("--port", default="19999", type=str)
+    base_subparser.add_argument("--api-version", default="3.0", type=str)
+    base_subparser.add_argument("--config-file", type=str)
+
+    return base_subparser
+
+
+def _build_connect_subparser(subparsers, base_subparser):
+    connect_sub = subparsers.add_parser(
+        "connect",
+        parents=[base_subparser],
+        help="Connect to Looker instance to test credentials.",
+    )
+    return connect_sub
+
+
+def _build_sql_subparser(subparsers, base_subparser):
+    sql_sub = subparsers.add_parser(
+        "sql",
+        parents=[base_subparser],
+        help="Build and run queries to test your Looker instance.",
+    )
+
+    sql_sub.add_argument("--project", default=os.environ.get("LOOKER_PROJECT"))
+    sql_sub.add_argument("--branch", default=os.environ.get("LOOKER_GIT_BRANCH"))
+    sql_sub.add_argument("--batch", action="store_true")
+
+    return sql_sub
+
+
+def parse_args(parser):
+    args = parser.parse_args()
+
+    if args.config_file:
+        data = yaml.load(args.config_file)
+        arg_dict = args.__dict__
+        for key, value in data.items():
+            if isinstance(value, list):
+                arg_dict[key].extend(value)
+            else:
+                arg_dict[key] = value
+    return args
+
+
+def connect(base_url, client_id, client_secret, port, api_version):
     client = Fonz(base_url, client_id, client_secret, port, api_version)
     client.connect()
 
 
-@click.command(cls=CommandWithConfig)
-@click.option("--project", envvar="LOOKER_PROJECT")
-@click.option("--branch", envvar="LOOKER_GIT_BRANCH")
-@click.option("--base-url", envvar="LOOKER_BASE_URL")
-@click.option("--client-id", envvar="LOOKER_CLIENT_ID")
-@click.option("--client-secret", envvar="LOOKER_CLIENT_SECRET")
-@click.option("--config-file")
-@click.option("--port", default=19999)
-@click.option("--api-version", default="3.0")
-@click.option("--batch/--no-batch", default=False)
-def sql(
-    project,
-    branch,
-    base_url,
-    client_id,
-    client_secret,
-    config_file,
-    port,
-    api_version,
-    batch,
-):
+def sql(project, branch, base_url, client_id, client_secret, port, api_version, batch):
     client = Fonz(
         base_url, client_id, client_secret, port, api_version, project, branch
     )
@@ -95,8 +142,5 @@ def sql(
     client.report_results(batch)
 
 
-cli.add_command(connect)
-cli.add_command(sql)
-
 if __name__ == "__main__":
-    cli()
+    main()
