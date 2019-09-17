b98c07191f2c21bc7bc59c1002d5b5da2032a7d3
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-09-17 14:25:16 -0400

    Add more attributes to SqlError

--- fonz/exceptions.py
+++ fonz/exceptions.py
@@ -14,11 +14,20 @@ class QueryNotFinished(FonzException):
     pass
 
 
-class SqlError(FonzException):
-    def __init__(self, message, sql, line_number):
-        self.message = message
+class SqlError(ValidationError):
+    def __init__(
+        self,
+        path: str,
+        message: str,
+        sql: str,
+        line_number: int = None,
+        url: str = None,
+    ):
+        super().__init__(message)
+        self.path = path
         self.sql = sql
         self.line_number = line_number
+        self.url = url
 
     def __repr__(self):
         return self.message
