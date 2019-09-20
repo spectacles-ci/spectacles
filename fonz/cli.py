4a7884c649ece0d24209e65e68bf8a69e7b4fd48
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-09-20 10:49:38 -0400

    Create custom ConfigAction to load arguments from file

--- fonz/cli.py
+++ fonz/cli.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import sys
 import yaml
 import argparse
@@ -6,6 +7,26 @@ from fonz.runner import Runner
 from fonz.client import LookerClient
 from fonz.exceptions import FonzException, ValidationError
 from fonz.logger import GLOBAL_LOGGER as logger, LOG_FILEPATH
+
+
+class ConfigAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        config = self.parse_config(path=values)
+        for key, value in config.items():
+            setattr(namespace, key, value)
+        parser.set_defaults(**config)
+
+    def parse_config(self, path):
+        raise NotImplementedError()
+
+
+class YamlConfigAction(ConfigAction):
+    def parse_config(self, path):
+        try:
+            with Path(path).open("r") as file:
+                return yaml.safe_load(file)
+        except (FileNotFoundError, yaml.parser.ParserError) as error:
+            raise argparse.ArgumentError(self, error)
 
 
 def handle_exceptions(function):
@@ -36,28 +57,8 @@ def handle_exceptions(function):
 
 @handle_exceptions
 def main():
-    parser = argparse.ArgumentParser(prog="fonz")
-    subparser_action = parser.add_subparsers(
-        title="Available sub-commands", dest="command"
-    )
-    base_subparser, defaults = _build_base_subparser()
-    subparsers = {
-        "connect": _build_connect_subparser(subparser_action, base_subparser),
-        "sql": _build_sql_subparser(subparser_action, base_subparser, defaults),
-    }
-
+    parser = create_parser()
     args = parser.parse_args()
-
-    # Check arguments for missing required arguments and error the correct subparser
-    required_args = ["base_url", "client_id", "client_secret", "project", "branch"]
-    if not all(getattr(args, arg_name, True) for arg_name in required_args):
-        null_arg_names = [
-            arg_name for arg_name in required_args if not getattr(args, arg_name, True)
-        ]
-        subparsers[args.command].error(
-            "the following arguments are required: "
-            f"{', '.join('--' + name for name in null_arg_names)}"
-        )
 
     if args.command == "connect":
         connect(
@@ -81,35 +82,27 @@ def main():
         )
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(prog="fonz")
+    subparser_action = parser.add_subparsers(
+        title="Available sub-commands", dest="command"
+    )
+    base_subparser = _build_base_subparser()
+    _build_connect_subparser(subparser_action, base_subparser),
+    _build_sql_subparser(subparser_action, base_subparser)
+    return parser
+
+
 def _build_base_subparser():
     base_subparser = argparse.ArgumentParser(add_help=False)
-    base_subparser.add_argument("--config-file")
-    args, remaining = base_subparser.parse_known_args()
-    if args.config_file:
-        with open(args.config_file, "r") as file:
-            config_from_file = yaml.safe_load(file)
-    else:
-        config_from_file = {}
-
-    env_var_arg_map = {
-        "base_url": "LOOKER_BASE_URL",
-        "client_id": "LOOKER_CLIENT_ID",
-        "client_secret": "LOOKER_CLIENT_SECRET",
-        "project": "LOOKER_PROJECT",
-        "branch": "LOOKER_GIT_BRANCH",
-    }
-
-    defaults = {}
-    for arg, env_var in env_var_arg_map.items():
-        defaults[arg] = os.environ.get(env_var) or config_from_file.get(arg)
-
-    base_subparser.add_argument("--base-url", default=defaults["base_url"])
-    base_subparser.add_argument("--client-id", default=defaults["client_id"])
-    base_subparser.add_argument("--client-secret", default=defaults["client_secret"])
+    base_subparser.add_argument("--config-file", action=YamlConfigAction)
+    base_subparser.add_argument("--base-url", required=False)
+    base_subparser.add_argument("--client-id", required=False)
+    base_subparser.add_argument("--client-secret", required=False)
     base_subparser.add_argument("--port", type=int, default=19999)
     base_subparser.add_argument("--api-version", type=float, default=3.1)
 
-    return base_subparser, defaults
+    return base_subparser
 
 
 def _build_connect_subparser(subparser_action, base_subparser):
@@ -119,22 +112,18 @@ def _build_connect_subparser(subparser_action, base_subparser):
         help="Connect to Looker instance to test credentials.",
     )
 
-    return subparser
 
-
-def _build_sql_subparser(subparser_action, base_subparser, defaults):
+def _build_sql_subparser(subparser_action, base_subparser):
     subparser = subparser_action.add_parser(
         "sql",
         parents=[base_subparser],
         help="Build and run queries to test your Looker instance.",
     )
 
-    subparser.add_argument("--project", default=defaults["project"])
-    subparser.add_argument("--branch", default=defaults["branch"])
+    subparser.add_argument("--project", required=False)
+    subparser.add_argument("--branch", required=False)
     subparser.add_argument("--explores", nargs="+", default=["*.*"])
     subparser.add_argument("--batch", action="store_true")
-
-    return subparser
 
 
 def connect(base_url, client_id, client_secret, port, api_version):
