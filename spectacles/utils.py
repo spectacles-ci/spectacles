a7f567c29b3a9b877c32c9a41b638db43f41cb58
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-10-15 15:29:27 -0400

    Improve error messaging for API errors

--- spectacles/utils.py
+++ spectacles/utils.py
@@ -1,4 +1,5 @@
 from typing import List
+import requests
 
 
 def compose_url(base_url: str, path: List) -> str:
@@ -7,3 +8,15 @@ def compose_url(base_url: str, path: List) -> str:
     parts = [base_url] + path
     url = "/".join(str(part).strip("/") for part in parts)
     return url
+
+
+def details_from_http_error(response: requests.Response) -> str:
+    try:
+        response_json = response.json()
+    # Requests raises a ValueError if the response is invalid JSON
+    except ValueError:
+        details = ""
+    else:
+        details = response_json.get("message")
+    details = details.strip() if details else ""
+    return details
