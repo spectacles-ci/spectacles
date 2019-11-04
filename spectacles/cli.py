cb19ebfc0e1d86c4f75ced9e4be872d047194535
Dylan Baker <dylang.baker@googlemail.com>
2019-11-04 09:07:11 -0800

    mode help

--- spectacles/cli.py
+++ spectacles/cli.py
@@ -324,7 +324,14 @@ def _build_sql_subparser(
             'model_name.*' would select all explores in the 'model_name' model.",
     )
     subparser.add_argument(
-        "--mode", choices=["batch", "single", "hybrid"], default="batch"
+        "--mode",
+        choices=["batch", "single", "hybrid"],
+        default="batch",
+        help="Specify the mode the SQL validator should run.\
+        	In single-dimension mode, the SQL validator will run one query \
+        	per dimension. In batch mode, the SQL validator will create one \
+        	query per explore. In hybrid mode, the SQL validator will run in \
+        	batch mode and then run errored explores in single-dimension mode.",
     )
 
 
