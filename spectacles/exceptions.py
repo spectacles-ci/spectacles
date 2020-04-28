68d035b152177c79fe60da56d5de06193a667f6e
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2020-04-28 17:18:16 -0500

    Update exception definition to use new format

--- spectacles/exceptions.py
+++ spectacles/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Dict, Any, Optional
+
+
 class SpectaclesException(Exception):
     exit_code = 100
 
@@ -9,27 +12,22 @@ class ApiConnectionError(SpectaclesException):
 class ValidationError(SpectaclesException):
     exit_code = 102
 
-
-class SqlError(ValidationError):
     def __init__(
         self,
-        path: str,
+        model: str,
+        explore: str,
+        test: str,
         message: str,
-        sql: str,
-        explore_url: str,
-        line_number: int = None,
-        url: str = None,
+        metadata: Dict[str, Any],
     ):
-        super().__init__(message)
-        self.path = path
+        self.model = model
+        self.explore = explore
+        self.test = test
         self.message = message
-        self.sql = sql
-        self.explore_url = explore_url
-        self.line_number = line_number
-        self.url = url
+        self.metadata = metadata
 
     def __eq__(self, other):
-        if not isinstance(other, SqlError):
+        if not isinstance(other, self.__class__):
             return NotImplemented
 
         return self.__dict__ == other.__dict__
@@ -38,11 +36,34 @@ class SqlError(ValidationError):
         return self.message
 
 
-class DataTestError(ValidationError):
-    def __init__(self, path: str, message: str):
-        super().__init__(message)
-        self.path = path
-        self.message = message
+class SqlError(ValidationError):
+    def __init__(
+        self,
+        model: str,
+        explore: str,
+        sql: str,
+        message: str,
+        line_number: Optional[int] = None,
+        explore_url: Optional[str] = None,
+        lookml_url: Optional[str] = None,
+    ):
+        metadata = {
+            "line_number": line_number,
+            "explore_url": explore_url,
+            "lookml_url": lookml_url,
+        }
+        super().__init__(model, explore, sql, message, metadata)
 
-    def __repr__(self):
-        return self.message
+
+class DataTestError(ValidationError):
+    def __init__(
+        self,
+        model: str,
+        explore: str,
+        expression: str,
+        message: str,
+        test_name: str,
+        assert_name: str,
+    ):
+        metadata = {"test_name": test_name, "assert_name": assert_name}
+        super().__init__(model, explore, expression, message, metadata)
