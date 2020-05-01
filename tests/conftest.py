17bd6722dfe418bf7428768a4601475d1d4b1b8a
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2020-05-01 11:06:57 -0500

    Remove access token in response from auth

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 from typing import Iterable
 import os
+import json
 import vcr
 import pytest
 from spectacles.client import LookerClient
+
+
+def scrub_access_token(response):
+    body = json.loads(response["body"]["string"].decode())
+    body["access_token"] = ""
+    response["body"]["string"] = json.dumps(body).encode()
+    return response
 
 
 @pytest.fixture(scope="session")
@@ -16,6 +24,7 @@ def looker_client(record_mode) -> Iterable[LookerClient]:
         "tests/cassettes/init_client.yaml",
         filter_post_data_parameters=["client_id", "client_secret"],
         filter_headers=["Authorization"],
+        before_record_response=scrub_access_token,
         record_mode=record_mode,
     ):
         client = LookerClient(
