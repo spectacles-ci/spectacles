b6fdfb8e33b55da1f76139ac035d56ef7c96edc2
Dylan Baker <dylang.baker@googlemail.com>
2020-05-02 17:26:34 -0400

    runner tests for import projects error

---
+++ tests/test_runner.py
@@ -0,0 +1,15 @@
+import pytest
+from spectacles.runner import Runner
+from spectacles.exceptions import ApiConnectionError
+
+
+def test_validate_sql_with_import_projects_error(runner):
+    runner.import_projects = True
+    with pytest.raises(ApiConnectionError):
+        runner.validate_sql(["*/*"], [])
+
+
+def test_validate_assert_with_import_projects_error(runner):
+    runner.import_projects = True
+    with pytest.raises(ApiConnectionError):
+        runner.validate_sql()
