b72084f391d953d4aa1b067b6f8abd67a85c467f
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2020-06-20 09:49:51 -0500

    Move validators into their own package and modules

---
+++ spectacles/validators/__init__.py
@@ -0,0 +1,5 @@
+from spectacles.validators.sql import SqlValidator
+from spectacles.validators.data_test import DataTestValidator
+from spectacles.validators.content import ContentValidator
+
+__all__ = ["SqlValidator", "DataTestValidator", "ContentValidator"]
