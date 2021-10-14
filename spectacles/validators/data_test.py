cad6bf856fd3c48fde1b6343360eb95078e72780
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2021-10-14 16:55:40 -0700

    Rework data test validator to return errors directly

--- spectacles/validators/data_test.py
+++ spectacles/validators/data_test.py
@@ -1,10 +1,47 @@
-from typing import Optional, Dict, Any
-from spectacles.validators.validator import Validator
-from spectacles.lookml import Explore
+from dataclasses import dataclass
+from typing import List, Optional
+from spectacles.client import LookerClient
+from spectacles.lookml import Explore, Project
 from spectacles.exceptions import SpectaclesException, DataTestError
 
 
-class DataTestValidator(Validator):
+@dataclass
+class DataTest:
+    name: str
+    explore: Explore
+    project_name: str
+    base_url: str
+    query_url_params: str
+    file: str
+    line: int
+    passed: Optional[bool] = None
+
+    def __post_init__(self):
+        try:
+            self.file_path = self.file.split("/", 1)[1]
+        except IndexError:
+            raise SpectaclesException(
+                name="data-test-has-incorrect-file-path-format",
+                title="A data test does not have the correct file path format.",
+                detail=f"Couldn't extract file path from unexpected file '{self.file}'",
+            )
+
+    @property
+    def explore_url(self):
+        return (
+            f"{self.base_url}/explore/{self.explore.model_name}"
+            f"/{self.explore.name}?{self.query_url_params}"
+        )
+
+    @property
+    def lookml_url(self) -> str:
+        return (
+            f"{self.base_url}/projects/{self.project_name}"
+            f"/files/{self.file_path}?line={self.line}"
+        )
+
+
+class DataTestValidator:
     """Runs LookML/data tests for a given project.
 
     Args:
@@ -13,93 +50,69 @@ class DataTestValidator(Validator):
 
     """
 
-    def validate(self) -> Dict[str, Any]:
-        all_tests = self.client.all_lookml_tests(self.project.name)
+    def __init__(self, client: LookerClient):
+        self.client = client
+
+    def get_tests(self, project: Project) -> List[DataTest]:
+        all_tests = self.client.all_lookml_tests(project.name)
 
         # Filter the list of tests to those that are selected
-        selected_tests = []
-        # The error objects don't contain the name of the explore
-        # We create this mapping to help look up the explore from the test name
-        test_to_explore = {}
+        selected_tests: List[DataTest] = []
 
-        for test in all_tests:
-            model_name = test["model_name"]
-            explore_name = test["explore_name"]
-            explore: Optional[Explore] = self.project.get_explore(
-                model=model_name, name=explore_name
+        for result in all_tests:
+            explore = project.get_explore(
+                model=result["model_name"], name=result["explore_name"]
             )
 
             # Skip tests that are not associated with a selected explore
-            if explore is None:
-                continue
+            if explore:
+                test = DataTest(
+                    name=result["name"],
+                    explore=explore,
+                    project_name=project.name,
+                    base_url=self.client.base_url,
+                    query_url_params=result["query_url_params"],
+                    file=result["file"],
+                    line=result["line"],
+                )
 
-            selected_tests.append(test)
-            test_to_explore[test["name"]] = explore
+                selected_tests.append(test)
 
         if len(selected_tests) == 0:
             raise SpectaclesException(
                 name="no-data-tests-found",
                 title="No data tests found.",
                 detail=(
-                    "If you're using --explores or --exclude, make sure your project "
+                    "If you're using --explores, make sure your project "
                     "has data tests that reference those models or explores."
                 ),
             )
 
-        for test in selected_tests:
-            model_name = test["model_name"]
-            explore_name = test["explore_name"]
-            query_url_params = test["query_url_params"]
+        return selected_tests
 
-            try:
-                file_path = test["file"].split("/", 1)[1]
-            except IndexError:
-                raise SpectaclesException(
-                    name="data-test-has-incorrect-file-path-format",
-                    title="A data test does not have the correct file path format.",
-                    detail=f"Couldn't extract file path from unexpected file '{test['file']}'",
-                )
-
-            explore_url = (
-                f"{self.client.base_url}/explore/{model_name}"
-                f"/{explore_name}?{query_url_params}"
-            )
-            lookml_url = (
-                f"{self.client.base_url}/projects/{self.project.name}"
-                f"/files/{file_path}?line={test['line']}"
-            )
-
+    def validate(self, tests: List[DataTest]) -> List[DataTestError]:
+        data_test_errors: List[DataTestError] = []
+        for test in tests:
             results = self.client.run_lookml_test(
-                self.project.name, model=test["model_name"], test=test["name"]
+                test.project_name, model=test.explore.model_name, test=test.name
             )
-            explore = test_to_explore[test["name"]]
-            explore.queried = True
+            test.explore.queried = True
             result = results[0]  # For a single test, list with length 1
 
-            for error in result["errors"]:
-                explore.errors.append(
-                    DataTestError(
-                        model=error["model_id"],
-                        explore=error["explore"],
-                        message=error["message"],
-                        test_name=result["test_name"],
-                        lookml_url=lookml_url,
-                        explore_url=explore_url,
-                    )
-                )
-
-            # TODO: Refactor this into "test" objects
             if result["success"]:
-                explore.successes.append(
-                    {
-                        "model": model_name,
-                        "explore": explore_name,
-                        "metadata": {
-                            "test_name": result["test_name"],
-                            "explore_url": explore_url,
-                            "lookml_url": lookml_url,
-                        },
-                    }
-                )
+                test.passed = True
+            else:
+                test.passed = False
+                for error in result["errors"]:
+                    data_test_errors.append(
+                        DataTestError(
+                            model=error["model_id"],
+                            explore=error["explore"],
+                            message=error["message"],
+                            test_name=result["test_name"],
+                            lookml_url=test.lookml_url,
+                            explore_url=test.explore_url,
+                        )
+                    )
 
-        return self.project.get_results(validator="data_test")
+        return data_test_errors
