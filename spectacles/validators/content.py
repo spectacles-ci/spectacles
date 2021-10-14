b09e2159326cadf54e05aae48c94515c6aacdb8f
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2021-10-14 14:33:51 -0700

    Rework content validator to return errors directly
    Also removes the parent Validator class entirely, which was violating Liskov substitution principle and frankly wasn't very useful as a parent class anyway.

--- spectacles/validators/content.py
+++ spectacles/validators/content.py
@@ -1,22 +1,20 @@
 from typing import List, Optional, Any, Dict
-from spectacles.validators.validator import Validator
 from spectacles.client import LookerClient
 from spectacles.exceptions import ContentError, SpectaclesException
-from spectacles.lookml import Explore
+from spectacles.lookml import Explore, Project
 from spectacles.logger import GLOBAL_LOGGER as logger
 from spectacles.types import JsonDict
 
 
-class ContentValidator(Validator):
+class ContentValidator:
     def __init__(
         self,
         client: LookerClient,
-        project: str,
         exclude_personal: bool = False,
         exclude_folders: List[int] = [],
         include_folders: List[int] = [],
     ):
-        super().__init__(client, project)
+        self.client = client
         personal_folders = self._get_personal_folders() if exclude_personal else []
 
         self.excluded_folders: List[int] = personal_folders + self._get_all_subfolders(
@@ -26,7 +24,7 @@ class ContentValidator(Validator):
 
     def _get_personal_folders(self) -> List[int]:
         personal_folders = []
-        result = self.client.all_folders(self.project.name)
+        result = self.client.all_folders()
         for folder in result:
             if folder["is_personal"] or folder["is_personal_descendant"]:
                 personal_folders.append(folder["id"])
@@ -34,7 +32,7 @@ class ContentValidator(Validator):
 
     def _get_all_subfolders(self, input_folders: List[int]) -> List[int]:
         result = []
-        all_folders = self.client.all_folders(self.project.name)
+        all_folders = self.client.all_folders()
         for folder_id in input_folders:
             if not any(folder["id"] == folder_id for folder in all_folders):
                 raise SpectaclesException(
@@ -55,18 +53,19 @@ class ContentValidator(Validator):
                 subfolders.extend(self._get_subfolders(child, all_folders))
         return subfolders
 
-    def is_selected(self, folder_id: Optional[str]) -> bool:
-        if folder_id in self.excluded_folders:
-            return False
-        if self.included_folders and folder_id not in self.included_folders:
-            return False
-        else:
-            return True
+    def validate(self, project: Project) -> List[ContentError]:
+        def is_folder_selected(folder_id: Optional[str]) -> bool:
+            if folder_id in self.excluded_folders:
+                return False
+            if self.included_folders and folder_id not in self.included_folders:
+                return False
+            else:
+                return True
 
-    def validate(self) -> Dict[str, Any]:
         result = self.client.content_validation()
-        self.project.queried = True
+        project.queried = True
 
+        content_errors: List[ContentError] = []
         for content in result["content_with_errors"]:
             # Skip content dicts if they lack a `look` or `dashboard` key
             try:
@@ -82,12 +81,13 @@ class ContentValidator(Validator):
             # Sometimes the content no longer exists, in which case the folder is None
             folder_id: Optional[str] = content[content_type]["folder"].get("id")
             # If exclude_personal isn't specified, personal_folders list is empty
-            if not self.is_selected(folder_id):
+            if not is_folder_selected(folder_id):
                 continue
             else:
-                self._handle_content_result(content, content_type)
+                errors = self._get_errors_from_result(project, content, content_type)
+                content_errors.extend(errors)
 
-        return self.project.get_results(validator="content")
+        return content_errors
 
     @staticmethod
     def _get_content_type(content: Dict[str, Any]) -> str:
@@ -109,37 +109,38 @@ class ContentValidator(Validator):
                 "Tile type not found. Valid keys are 'dashboard_element', 'dashboard_filter'"
             )
 
-    def _handle_content_result(self, content: Dict, content_type: str) -> None:
-        for error in content["errors"]:
+    def _get_errors_from_result(
+        self, project: Project, result: Dict, content_type: str
+    ) -> List[ContentError]:
+        content_errors: List[ContentError] = []
+        for error in result["errors"]:
             model_name = error["model_name"]
             explore_name = error["explore_name"]
-            explore: Optional[Explore] = self.project.get_explore(
+            explore: Optional[Explore] = project.get_explore(
                 model=model_name, name=explore_name
             )
             # Skip errors that are not associated with selected explores
             if explore is None:
                 continue
 
-            content_id = content[content_type]["id"]
+            content_id = result[content_type]["id"]
             content_error = ContentError(
                 model=model_name,
                 explore=explore_name,
                 message=error["message"],
                 field_name=error["field_name"],
                 content_type=content_type,
-                title=content[content_type]["title"],
-                space=content[content_type]["space"]["name"],
+                title=result[content_type]["title"],
+                space=result[content_type]["space"]["name"],
                 url=f"{self.client.base_url}/{content_type}s/{content_id}",
                 tile_type=(
-                    self._get_tile_type(content)
-                    if content_type == "dashboard"
-                    else None
+                    self._get_tile_type(result) if content_type == "dashboard" else None
                 ),
                 tile_title=(
-                    content[self._get_tile_type(content)]["title"]
+                    result[self._get_tile_type(result)]["title"]
                     if content_type == "dashboard"
                     else None
                 ),
             )
-            if content_error not in explore.errors:
-                explore.errors.append(content_error)
+            content_errors.append(content_error)
+        return content_errors
