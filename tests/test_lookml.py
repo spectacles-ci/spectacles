64b30d0f29724a8410e27276689b192e8684797f
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2021-10-15 08:08:39 -0700

    Move build project tests to test_lookml

--- tests/test_lookml.py
+++ tests/test_lookml.py
@@ -1,7 +1,48 @@
 from copy import deepcopy
 import pytest
-from spectacles.lookml import Model, Explore, Dimension
+from spectacles.lookml import Model, Explore, Dimension, build_project
+from spectacles.exceptions import SpectaclesException
 from utils import load_resource
+
+
+@pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
+class TestBuildProject:
+    def test_model_explore_dimension_counts_should_match(self, looker_client):
+        project = build_project(
+            looker_client,
+            name="eye_exam",
+            filters=["eye_exam/users"],
+            include_dimensions=True,
+        )
+        assert len(project.models) == 1
+        assert len(project.models[0].explores) == 1
+        dimensions = project.models[0].explores[0].dimensions
+        assert len(dimensions) == 6
+        assert "users.city" in [dim.name for dim in dimensions]
+        assert not project.errored
+        assert project.queried is False
+
+    def test_project_with_everything_excluded_should_not_have_models(
+        self, looker_client
+    ):
+        project = build_project(looker_client, name="eye_exam", filters=["-eye_exam/*"])
+        assert len(project.models) == 0
+
+    def test_duplicate_selectors_should_be_deduplicated(self, looker_client):
+        project = build_project(
+            looker_client, name="eye_exam", filters=["eye_exam/users", "eye_exam/users"]
+        )
+        assert len(project.models) == 1
+
+
+@pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
+class TestBuildUnconfiguredProject:
+    """Test for a build error when building an unconfigured LookML project."""
+
+    def test_project_with_no_configured_models_should_raise_error(self, looker_client):
+        looker_client.update_workspace(workspace="production")
+        with pytest.raises(SpectaclesException):
+            build_project(looker_client, name="eye_exam_unconfigured")
 
 
 def test_model_from_json():
