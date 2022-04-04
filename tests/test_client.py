4f6159dfdaba94d84365ae8e9f878a66be342c53
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2022-04-04 11:08:03 -0400

    Update Looker version in test

--- tests/test_client.py
+++ tests/test_client.py
@@ -101,7 +101,7 @@ def client_kwargs():
 @pytest.mark.vcr
 def test_get_looker_release_version_should_return_correct_version(looker_client):
     version = looker_client.get_looker_release_version()
-    assert version == "7.6.17"
+    assert version == "22.4.29"
 
 
 @pytest.mark.vcr(filter_post_data_parameters=["client_id", "client_secret"])
