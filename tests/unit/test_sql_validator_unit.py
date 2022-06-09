ee40a42f90992b955da45a9b4020cd62b21a78b3
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2022-06-09 12:54:19 -0400

    Add another test for exception

--- tests/unit/test_sql_validator_unit.py
+++ tests/unit/test_sql_validator_unit.py
@@ -1,9 +1,11 @@
 import asyncio
 from typing import Optional
 import pytest
+import httpx
 import respx
 from spectacles.validators.sql import Query, SqlValidator
 from spectacles.lookml import Explore, Dimension
+from spectacles.exceptions import LookerApiError
 
 
 @pytest.fixture
@@ -86,7 +88,7 @@ async def test_run_query_works(
 
     queries_to_run: asyncio.Queue[Optional[Query]] = asyncio.Queue()
     running_queries: asyncio.Queue[str] = asyncio.Queue()
-    query_slot = asyncio.Semaphore()
+    query_slot = asyncio.Semaphore(1)
 
     task = asyncio.create_task(
         validator._run_query(queries_to_run, running_queries, query_slot)
@@ -98,6 +100,7 @@ async def test_run_query_works(
     # Have to manually mark the queue task as done, since normally this is handled by
     # `SqlValidator._get_query_results`
     queries_to_run.task_done()
+    query_slot.release()
     await queries_to_run.join()
     task.cancel()
 
@@ -111,7 +114,7 @@ async def test_run_query_works(
 async def test_run_query_shuts_down_on_sentinel(validator: SqlValidator):
     queries_to_run: asyncio.Queue[Optional[Query]] = asyncio.Queue()
     running_queries: asyncio.Queue[str] = asyncio.Queue()
-    query_slot = asyncio.Semaphore()
+    query_slot = asyncio.Semaphore(1)
 
     task = asyncio.create_task(
         validator._run_query(queries_to_run, running_queries, query_slot)
@@ -120,3 +123,53 @@ async def test_run_query_shuts_down_on_sentinel(validator: SqlValidator):
     await queries_to_run.put(None)
     await queries_to_run.join()
     await asyncio.gather(task)
+
+
+async def test_run_query_handles_exceptions_raised_within(
+    mocked_api: respx.MockRouter,
+    explore: Explore,
+    dimension: Dimension,
+    validator: SqlValidator,
+):
+    query_id = 12345
+    query_task_id = "abcdef12345"
+    explore_url = "https://spectacles.looker.com/x"
+
+    mocked_api.post(
+        "queries", params={"fields": "id,share_url"}, name="create_query"
+    ).mock(
+        side_effect=(
+            httpx.Response(200, json={"id": query_id, "share_url": explore_url}),
+            httpx.Response(404),
+        )
+    )
+
+    mocked_api.post(
+        "query_tasks",
+        params={"fields": "id", "cache": "false"},
+        name="create_query_task",
+    ).respond(200, json={"id": query_task_id})
+
+    queries_to_run: asyncio.Queue[Optional[Query]] = asyncio.Queue()
+    running_queries: asyncio.Queue[str] = asyncio.Queue()
+    query_slot = asyncio.Semaphore(1)
+
+    task = asyncio.create_task(
+        validator._run_query(queries_to_run, running_queries, query_slot)
+    )
+
+    query = Query(explore, (dimension,), query_id)
+    queries_to_run.put_nowait(query)  # This will succeed
+    queries_to_run.put_nowait(query)  # This will fail with 404
+    await running_queries.get()  # Retrieve the successfully query
+
+    # Normally these steps are handled by _get_query_results
+    queries_to_run.task_done()
+    query_slot.release()
+    await queries_to_run.join()
+
+    with pytest.raises(LookerApiError):
+        await asyncio.gather(task)
+
+    assert running_queries.empty
+    mocked_api["create_query"].calls.assert_called()
