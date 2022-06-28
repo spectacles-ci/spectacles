60ec49e4141204687629447c1868d0e64c57446b
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2022-06-28 11:13:48 -0400

    Fix data test validator tests

--- tests/integration/test_data_test_validator.py
+++ tests/integration/test_data_test_validator.py
@@ -1,67 +1,59 @@
-from typing import Iterable, List
 import pytest
 from spectacles.validators import DataTestValidator
 from spectacles.validators.data_test import DataTest
+from spectacles.client import LookerClient
 from spectacles.exceptions import DataTestError, SpectaclesException
 from spectacles.lookml import build_project
 
 
-@pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
-@pytest.fixture(scope="class")
-def validator(looker_client) -> Iterable[DataTestValidator]:
-    validator = DataTestValidator(looker_client)
-    yield validator
+@pytest.fixture
+def validator(looker_client: LookerClient) -> DataTestValidator:
+    return DataTestValidator(looker_client)
 
 
-class TestValidatePass:
-    """Test the eye_exam Looker project on master for an explore without errors."""
+@pytest.fixture(params=["no_errors", "errors"])
+async def tests(
+    request: pytest.FixtureRequest, validator: DataTestValidator
+) -> list[DataTest]:
+    if request.param == "no_errors":  # type: ignore[attr-defined]
+        explore_name = "users"
+    else:
+        explore_name = "users__fail"
 
-    @pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
-    @pytest.fixture(scope="class")
-    def tests(self, validator) -> Iterable[List[DataTest]]:
-        project = build_project(
-            validator.client, name="eye_exam", filters=["eye_exam/users"]
+    project = await build_project(
+        validator.client, name="eye_exam", filters=[f"eye_exam/{explore_name}"]
+    )
+    tests = await validator.get_tests(project)
+    return tests
+
+
+@pytest.fixture
+async def validation_result(
+    validator: DataTestValidator, tests: list[DataTest]
+) -> tuple[DataTestError, ...]:
+    errors = await validator.validate(tests)
+    return tuple(errors)
+
+
+def test_correct_number_of_tests_generated(tests):
+    assert len(tests) == 2
+
+
+def test_correct_number_of_errors_returned(
+    validation_result: tuple[DataTestError, ...]
+):
+    # Errors should only come from the failing Explore
+    if validation_result:
+        assert len(validation_result) == 3
+        print([error.explore for error in validation_result])
+        assert all(
+            error.explore in ("users__fail", None) for error in validation_result
         )
-        tests: List[DataTest] = validator.get_tests(project)
-        yield tests
-
-    @pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
-    @pytest.fixture(scope="class")
-    def validator_errors(self, validator, tests):
-        errors: List[DataTestError] = validator.validate(tests)
-        yield errors
-
-    def test_results_have_correct_number_of_elements(self, validator_errors, tests):
-        assert len(validator_errors) == 0
-        assert len(tests) == 2
 
 
-class TestValidateFail:
-    """Test the eye_exam Looker project on master for an explore without errors."""
-
-    @pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
-    @pytest.fixture(scope="class")
-    def tests(self, validator) -> Iterable[List[DataTest]]:
-        project = build_project(
-            validator.client, name="eye_exam", filters=["eye_exam/users__fail"]
-        )
-        tests: List[DataTest] = validator.get_tests(project)
-        yield tests
-
-    @pytest.mark.vcr(match_on=["uri", "method", "raw_body"])
-    @pytest.fixture(scope="class")
-    def validator_errors(self, validator, tests):
-        errors: List[DataTestError] = validator.validate(tests)
-        yield errors
-
-    def test_results_have_correct_number_of_elements(self, validator_errors, tests):
-        assert len(validator_errors) == 3
-        assert len(tests) == 2
-        assert len(list(test for test in tests if test.passed)) == 0
-
-
-@pytest.mark.vcr
-def test_no_data_tests_should_raise_error(validator):
+async def test_no_data_tests_should_raise_error(validator: DataTestValidator):
     with pytest.raises(SpectaclesException):
-        project = build_project(validator.client, name="eye_exam", filters=["-*/*"])
-        validator.get_tests(project)
+        project = await build_project(
+            validator.client, name="eye_exam", filters=["-*/*"]
+        )
+        await validator.get_tests(project)
